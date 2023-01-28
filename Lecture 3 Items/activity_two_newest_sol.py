0d21c69afcdd640d627a90180c13322f75386042
Rachel DuBois <rachel@duboishouse.com>
2023-01-27 19:44:18 -0500

    Create activity_two_newest_sol.py

---
+++ Lecture 3 Items/activity_two_newest_sol.py
@@ -0,0 +1,44 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+# Create a figure and axes
+fig = plt.figure(figsize=(6,6))
+ax = plt.subplot(111)
+
+ax.set_xlim((-2, 2))
+ax.set_ylim((-2, 2))
+ax.set_xlabel('X')
+ax.set_ylabel('Y')
+ax.set_title('earth movement with transformation')
+
+# Use two points (a vector) to represent earth orientation and location
+earth = np.array([[0,0],[1,1.2],[1,1]])
+n_step = 1000
+delta_theta = 0.01
+
+# Movement items
+sun, = ax.plot([0], [0], 'r.', ms=50)
+earth_loc, = ax.plot(earth[0,0], earth[1,0], 'g.', ms=20)
+earth_orient, = ax.plot(earth[0,:], earth[1,:], 'g', lw=2)
+
+# Animation function
+def update_draw(frames):
+    t = np.identity(3)
+    movement = np.array([[np.cos(delta_theta), np.sin(delta_theta), 0], [-np.sin(delta_theta), np.cos(delta_theta), 0], [0,0,1]])
+    for i in range(frames):
+        t = np.matmul(t, movement)
+    pose = np.matmul(t, earth)
+    earth_loc.set_data(pose[0,0], pose[1,0])
+    earth_orient.set_data(pose[0,:], pose[1,:])
+    return (earth_orient,)
+
+# Show animation
+from matplotlib import rc
+from matplotlib import animation
+
+# Blit=True re-draws only the parts that have changed
+anim = animation.FuncAnimation(fig, update_draw, frames=n_step, interval=20, blit=True)
+
+# Equivalent to rcParams['animation.html'] = 'html5'
+rc('animation', html='html5')
+anim
